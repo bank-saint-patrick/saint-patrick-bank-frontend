ce1183bf2b507572912afe520907ea34150157e2
Irwing Naranjo <khalisser@gmail.com>
2022-03-17 00:33:42 -0300

    inicio nuevo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
