dbfcb7def8a6f0dffd93663d57d0488dcda8b40b
Irwing Naranjo <khalisser@gmail.com>
2022-03-18 21:57:58 -0300

    Clear with eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
+import "./App.css";
 
 function App() {
   return (
