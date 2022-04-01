048f41217bada18166ed2c88312464c9f9d7ad08
becarrillo <77902818+becarrillo@users.noreply.github.com>
2022-04-01 17:06:43 -0500

    página Sobre Nosotros realizada

--- src/components/website/AboutUs/index.jsx
+++ src/components/website/AboutUs/index.jsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../../Navbar';
 
 export default function AboutUs() {
   return (
     <div className="wrapper flex-column h-screen">
       <Navbar />
-      <div className="bg-lime-100 full-height-conditional grid grid-cols-1">
-        <div className="bg-white flex flex-col justify-center px-10">
-          <span className="text-plantation text-4xl text-center font-bold mb-2">Sobre nosotros</span>
+      <div className="container mx-auto mt-5 full-height-conditional w-auto">
+        <div className='grid grid-cols-1 text-left xsm:ml-7 lg:ml-5'>
+          <h4 className='bg-gray-100 shadow-md shadow-zinc-300 text-3xl text-dingley font-semibold'>Sobre Nosotros</h4>
+          <p className='text-lg my-4'>
+            Banco Saint Patrick es una plataforma web de educación financiera creada con la finalidad de promover la inclusión finan-<br></br>ciera
+            y digital de los adultos mayores en el país.
+          </p>
+          <p className='text-lg my-3'>
+            Nuestro objetivo es brindar herramientas que permitan facilitar el acceso de aprendizaje de los productos<br></br>
+            y servicios bancarios a través de un lenguaje claro y herramientas dinámicas.
+          </p>
+          <p  className='text-lg my-3'>
+            Nuestro compromiso es cuidarte en todo momento. Queremos que te sientas acompañado, comprendido, escuchado y que<br></br>
+            puedas tomar contacto con una persona real para responder tus dudas y consultas de forma personalizada en todo momen-<br></br>to.
+          </p>
+          <p className='text-lg my-3'>
+            Además vas a poder seleccionar por qué medio preferís que un asesor te contacte si necesitas nuestra ayuda.
+          </p>
+          <p className='text-lg my-3'> 
+            Si con los tutoriales y nuestro sector de soporte en línea todavía te quedan dudas sobre cómo se<br></br>
+            utiliza nuestra plataforma. ¡No te preocupes!
+          </p>
+          <p className='text-lg my-3'>
+            Vas a poder solicitar un turno para acercarte a nuestras sucursales el día y horario que tengas<br></br>
+            disponible para que nuestros <Link to='/#'><span className='underline text-desert hover:text-orange-600 mr-1'>Asesores guía</span></Link>
+            te muestren el paso a paso<br></br>
+            de cada transacción en nuestra área de capacitación.
+          </p>
         </div>
+        <span className='text-2xl text-cream-can font-semibold  xsm:ml-7 lg:ml-5'>Te acompañamos en éste desafío, anímate a aprender con nosotros!</span>
       </div>
     </div>
   );
--- src/components/website/Home/index.jsx
+++ src/components/website/Home/index.jsx
@@ -9,17 +9,16 @@ export default function Home() {
       <Navbar />
       <div className="full-height-conditional grid grid-cols-1 my-3 bg-adult bg-no-repeat" id='adultBg'>
         <section className="container flex flex-col mx-auto items-center">
-          <div className='w-full text-center ml-2 mb-3' id>
+          <div className='w-full text-center ml-2 mb-1'>
             <h2 className='text-3xl text-dingley mb-3 pl-3 ml-20'>INICIO BANCARIO</h2>
             <hr className='invisible sm:visible md:visible lg:visible xl:visible'></hr>
             <div className='container grid grid-cols-4 bg-no-repeat'>
-              <p className='col-span-3 ml-80 mt-14 w-auto py-20 text-lg font-bold' id='paragraph1'>
+              <p className='col-span-3 ml-80 mt-10 w-auto py-20 text-lg font-bold' id='paragraph1'>
                 Aprende a usar el sistema de banca en línea desde
                 una<br></br>computadora, tablet o celular. ¡Si tienes más dudas<br></br>ponte en contacto con nosotros!
                 <img src="https://img.icons8.com/external-royyan-wijaya-detailed-outline-royyan-wijaya/48/000000/external-arrow-arrow-line-royyan-wijaya-detailed-outline-royyan-wijaya-6.png" alt='arrow not found' className='mt-5 ml-44 pl-80  hover:translate-y-3.5 ease-out duration-75'/>
               </p>
             </div>
-            
           </div>
           
           <div className="flex flex-row bg-cream-can mb-5 p-4 h-96 shadow-md shadow-slate-600 w-full h-9/11 sm:w-9/11 md:w-3/4 lg:w-5/6 lg:h-80 xl:w-2/3 xl:h-80 2xl:w-1/2 2xl:h-80 2xl:mb-12">
@@ -34,7 +33,7 @@ export default function Home() {
                 ¿Quieres que te acompañemos en tu<br></br>primer experiencia?
               </p>
               <button className='bg-plantation rounded-lg px-5 py-1 shadow-sm shadow-zinc-500 text-xl font-semibold hover:bg-green-900 hover:ring-1 ring-dingley text-white my-7 lg:px-4 lg:py-1'>
-                SI DESEO ASESORAMIENTO
+                SÍ, DESEO ASESORAMIENTO
               </button>
             </div>
           </div>
